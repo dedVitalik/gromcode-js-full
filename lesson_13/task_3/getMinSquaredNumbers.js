3eca3b0b5953bcf0a43c6f76429436bdc18104df
Vitaliy Vynnyk <vitaliy.vinnik@gmail.com>
2022-05-11 18:55:46 +0200

    my jest tests with modules

--- lesson_13/task_3/getMinSquaredNumbers.js
+++ lesson_13/task_3/getMinSquaredNumbers.js
@@ -2,10 +2,6 @@ export default arr => {
     if (!Array.isArray(arr) || arr.length === 0) return null;
     return arr.reduce((prev, cur) => {
         const currentRes = Math.pow(Math.abs(cur), 2);
-        if (currentRes < prev) {
-            return  currentRes
-        } else {
-            return prev
-        }
+        return currentRes < prev ? currentRes : prev;
     }, Math.pow(Math.abs(arr[0]), 2));
 }
