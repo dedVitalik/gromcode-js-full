e3a7c55c4e77080ef54991199cd937f6d4587bfb
Vitaliy Vynnyk <vitaliy.vinnik@gmail.com>
2022-05-11 18:44:05 +0200

    my jest tests with modules

--- lesson_13/task_3/getMinSquaredNumbers.js
+++ lesson_13/task_3/getMinSquaredNumbers.js
@@ -1,4 +1,5 @@
 export default arr => {
+    if (!Array.isArray(arr) || arr.length === 0) return null;
     return arr.reduce((prev, cur) => {
         const currentRes = Math.pow(Math.abs(cur), 2);
         if (currentRes < prev) {
--- lesson_13/task_3/getMinSquaredNumbers.test.js
+++ lesson_13/task_3/getMinSquaredNumbers.test.js
@@ -1,7 +1,11 @@
 import minSqModuleOfArray from './getMinSquaredNumbers.js';
 
 it('should ', function () {
-    const res = minSqModuleOfArray([-777, 3, -2, 6, 45, -20]);
-    expect(res).toEqual(4);
+    const resIfArray = minSqModuleOfArray([-777, 3, -2, 6, 45, -20]);
+    expect(resIfArray).toEqual(4);
+    const resIfNullLength = minSqModuleOfArray([]);
+    expect (resIfNullLength).toEqual(null);
+    const resIfNotArray = minSqModuleOfArray('str');
+    expect (resIfNotArray).toEqual(null);
 });
 
