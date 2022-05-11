947a715a04a0e004bdec28a2a453988d2e604078
Vitaliy Vynnyk <vitaliy.vinnik@gmail.com>
2022-05-11 12:15:42 +0200

    my jest tests

--- lesson_13/index.test.js
+++ lesson_13/index.test.js
@@ -3,6 +3,14 @@ it('should show that 17 = 17', () => {
 });
 
 it('should show that 18 isn\'t 17', () => {
-  expect(18).not.toEqual(17);
+    expect(18).not.toEqual(17);
 });
 
+const getEvenNumbers = arr => {
+    return arr.filter(number => number % 2 === 0);
+}
+
+it('should ', function () {
+    const result = getEvenNumbers([1, 2, 3, 4, 5, 6]);
+    expect(result).toEqual([2, 4, 6]);
+});
